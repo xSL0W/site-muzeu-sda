073c90145a58c5a8d971538c8ed242288a98f01c
xSL0W <dolphinytb@gmail.com>
2022-12-18 21:12:22 +0200

    revert

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
 
-8uiyh7yg7ygt7g
+ 
 
 <style>
 .gradient-custom 
